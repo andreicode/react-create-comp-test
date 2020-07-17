81ed61210b181d0ddd393de4d999ea7a2964c4ff
Iulian <iulian.fundacescu@expertnetwork.ro>
2020-07-17 20:29:10 +0300

    remove unused code

--- src/test/simple-test/SimpleTest.js
+++ src/test/simple-test/SimpleTest.js
@@ -20,26 +20,11 @@ class SimpleTest extends React.Component {
         <br />
         <button data-testid="disabledBtn" disabled onClick={() => alert('clicked disabled btn')}>Cannot click me</button>
         <br />
-        <button data-testid="enableBtn" onClick={() => this.props.number++ }>Can click me</button>
-        <br />
-        <span data-testid="btnMessage"> {this.props.number}</span>
       </div>
     );
   }
 };
 
 // Can we have same test without using a class component?
-
-//  const SimpleTest = (props) => {
-//   let id = idCounter++;
-//   return (
-//     <div className="container">
-//       <p>SimpleTest</p>
-//       Display number: <span data-testid="number-display">{props.number}</span>
-//       <br/>
-//       Instance id: <span data-testid="instance-id">{id}</span>
-//     </div>
-//   );
-// };
 
 export default SimpleTest;
